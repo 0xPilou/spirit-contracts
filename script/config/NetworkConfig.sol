6c128b65a921eac5427be0814057be8ce8791440
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-10-10 15:10:47 +0200

    fix: remove `weth` from config (not required)

--- script/config/NetworkConfig.sol
+++ script/config/NetworkConfig.sol
@@ -14,7 +14,6 @@ library NetworkConfig {
         address positionManager;
         address poolManager;
         address permit2;
-        address weth;
         // SPIRIT Token & Liquidity Settings
         string spiritTokenName;
         string spiritTokenSymbol;
@@ -50,7 +49,6 @@ library NetworkConfig {
             positionManager: 0x7C5f5A4bBd8fD63184577525326123B519429bDc,
             poolManager: 0x498581fF718922c3f8e6A244956aF099B2652b2b,
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
-            weth: 0x4200000000000000000000000000000000000006,
             spiritTokenName: "Spirit Token",
             spiritTokenSymbol: "SPIRIT",
             spiritTokenSupply: 1_000_000_000 ether,
@@ -78,7 +76,6 @@ library NetworkConfig {
             positionManager: 0x4B2C77d209D3405F41a037Ec6c77F7F5b8e2ca80,
             poolManager: 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408,
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
-            weth: 0x4200000000000000000000000000000000000006,
             spiritTokenName: "Secret Token",
             spiritTokenSymbol: "SECRET",
             spiritTokenSupply: 1_000_000_000 ether,
@@ -101,7 +98,6 @@ library NetworkConfig {
             poolManager: 0xE03A1074c86CFeDd5C142C4F04F1a1536e203543,
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
             superTokenFactory: 0x254C2e152E8602839D288A7bccdf3d0974597193,
-            weth: 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14,
             spiritTokenName: "Secret Token",
             spiritTokenSymbol: "SECRET",
             spiritTokenSupply: 1_000_000_000 ether,
